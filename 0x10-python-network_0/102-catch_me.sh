683fd07bd948388b1bab974764890b28a90761bf
Prideland-Okoi <prideland.okoi@gmail.com>
2022-10-20 22:43:48 -0700

    header changed

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "origin: HolbertonSchool" -d "user_id=98"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -H "origin X-School-User-Id" -d "user_id=98"
