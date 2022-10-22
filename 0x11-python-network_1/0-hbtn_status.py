e0acef1e2681644b08a98f63a92ba162a9175a51
Prideland-Okoi <prideland.okoi@gmail.com>
2022-10-21 21:05:59 -0700

    task file created

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ A Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
-
+import sys
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
