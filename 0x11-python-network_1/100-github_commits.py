37332f8588a94e2848c17ed0d5a7fef544d264f5
Prideland-Okoi <prideland.okoi@gmail.com>
2022-10-25 15:49:22 -0700

    header and param added

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,24 +3,12 @@
 A Python script that shows the last 10 commits of a repository
 in GitHub
 """
-
-from requests import get
-from sys import argv
-
-
-if __name__ == "__main__":
-    repo = sys.argv[1]
-    owner = sys.argv[2]
-    headers = {
-            'Accept': 'application/vnd.github.v3+json',
-            }
-    params = {
-            'page': 10,
-            }
-
-    url = requests.get('https://api.github.com/repos/{}/{}/commits'.format(
-        owner, repo),
-        headers=headers, params=params)
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    url = requests.get('https://api.github.com/repos/{}/{}/commits'
+                       .format(argv[2], argv[1]))
     json_o = url.json()
-    for commit in json_o:
-        print(commit['sha'] + ':', commit['commit']['author']['name'])
+    for commit in json_o[:10]:
+        print(commit.get('sha'), end=':')
+        print(commit.get('commit').get('author').get('name'))
